0544bb58d281911a8ddaed9c13b77957eb6a3532
tan <1931980954@qq.com>
2021-01-29 13:14:02 +0800

    第一题

--- week1/src/task1.java
+++ week1/src/task1.java
@@ -1,5 +1,13 @@
 public class task1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int n = fab(1, 2);
+        System.out.println(n);
+    }
+    public static int fab(int a,int b)
+    {
+        if(a>b)
+            return 0;
+        return 1;
     }
 }
